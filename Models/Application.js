7e0c6e2fbcf50a88e6007ed2bd777a9f97687cc1
Ryan Mennemeier <kalpha1865@gmail.com>
2023-03-11 14:48:00 -0500

    updated model data and added lint/prettier files

--- Models/Application.js
+++ Models/Application.js
@@ -6,7 +6,62 @@ class Application extends Model {}
 
 Application.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: User,
+        key: "id"
+      },
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    ssn: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+
+    },
+    income: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      isInt: {
+        msg: "Must be an integer number of dollars"
+      }
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    credit: {
+      type: DataTypes.INTEGER,
+      min: 300,
+      max: 850
+    },
+    loan_type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: true,
+      isIn: {
+        args: [['Auto', 'Residential', 'Personal']],
+        msg: "Must be an Auto, Home or Personal Loan"
+      }
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
   },
   {
     sequelize,
