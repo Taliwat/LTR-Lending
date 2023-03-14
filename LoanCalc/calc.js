7c29a388b780ffee86a62fbcbfe642e928732475
Ryan Mennemeier <kalpha1865@gmail.com>
2023-03-14 14:06:42 -0400

    adding finishing touches to LoanCalc

--- LoanCalc/calc.js
+++ LoanCalc/calc.js
@@ -5,11 +5,11 @@ const downPayment = document.getElementById("downPayment");
 const loanAmount = document.getElementById("loanAmount");
 const interestRate = document.getElementById("interestRate");
 const loanDuration = document.getElementById("loanDuration");
-const loan = document.getElementById("loan");
+const form = document.getElementById("loan");
 
-//console.log(startingValue, downPayment, loanAmount, interestRate, loanDuration, loan);
+//console.log(startingValue, downPayment, loanAmount, interestRate, loanDuration, form);
 
-//write out basic function calcs for each field
+//write out basic function calcs for each field (research)
 downPayment.addEventListener("keyup", () => {
     loanAmount.value = startingValue.value - downPayment.value;
 
@@ -17,30 +17,35 @@ downPayment.addEventListener("keyup", () => {
     return loanAmountValue;
 });
 
+
 function calcLoan (loanAmount, interestRate, monthlyPayments) {
     function percentToDec(percent) {
         return percent / 12 / 100;
     }
     interestRate = percentToDec(interestRate);
-
+    
     function convertYearstoMonths(year) {
         return year * 12;
     }
     monthlyPayments = convertYearstoMonths(monthlyPayments);
-
-    let loan = (interestRate * loanAmount)/(1-Math.pow(1+interestRate, -monthlyPayments))
+    
+    let loan = (interestRate * loanAmount)/(1-Math.pow(1+interestRate,-monthlyPayments))
     return parseFloat(loan.toFixed(2));
 }
 
+//call the formulas to show in mon payment field.
 form.onsubmit = (e) => {
     e.preventDefault();
 
+
     let loanAmount = startingValue.value - downPayment.value;
-    calcLoan(loanAmount, interestRate.value, loanDuration.value);
+    let monthlyPayment = calcLoan(loanAmount, interestRate.value, loanDuration.value);
 
-    return console.log(loanAmount());
+    document.getElementById("monthlyPayment").innerHTML= `$ ${monthlyPayment}`;
+
+    console.log(loanAmount, interestRate.value, loanDuration.value);
+    
 }
-
 
 
 
